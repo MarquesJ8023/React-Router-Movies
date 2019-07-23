1ebce7e302488ec5361745c2fdc59936a7fc0037
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-23 07:43:22 -0400

    done and done

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList.js';
+import Movie from './Movies/Movie.js';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [
+		savedList,
+		setSavedList
+	] = useState([]);
+  //eslint-disable-next-line
+	const addToSavedList = (movie) => {
+		setSavedList([
+			...savedList,
+      movie
+    
+    ]);
+    
+	};
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
-
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<Switch>
+				<Route exact path="/" component={MovieList} />
+				<Route path="/movies/:id" component={Movie} />
+			</Switch>
+		</div>
+	);
 };
 
 export default App;
